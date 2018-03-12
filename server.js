330367216d2487eb2d4cb6eacfd21765b260e9c5
Francesco Bianco <info.francescobianco@gmail.com>
2018-03-12 05:18:56 +0100

    asd

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.post('/api/sms/push', function (req, res) {
-    fs.writeFileSync('sms.json', req.body);
+    fs.writeFileSync('sms.json', JSON.stringify(req.body));
     res.send(req.body);
 });
 
