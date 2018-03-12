c1832d6e0269487c56a4ab74dfb0b9e631e89e9f
Francesco Bianco <info.francescobianco@gmail.com>
2018-03-12 05:00:23 +0100

    asd

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('Express Hello World!');
+app.get('/sms/push', function (req, res) {
+    fs.writeFileSync('sms.json', req.body);
+    res.send(req.body);
 });
 
 app.listen(port, function () {
